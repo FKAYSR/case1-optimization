0d7dccc99b0a50529be7bc33e3082ea49ba1bbcb
FKAYSR <kejserfreja@gmail.com>
2026-08-27 16:36:06 +0200

    Alt indhold fra template slettet på tværs af flere filer.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pkg from "./package.json" with { type: "json" };
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
