e1749b69551baa9cf591c7d852af941ec4300437
FKAYSR <kejserfreja@gmail.com>
2026-09-02 21:51:10 +0200

    Button added in bottom right corner, to quickly be able to get to the top again.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useLocation } from "react-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
@@ -18,11 +18,47 @@ function ScrollToTop() {
   return null;
 }
 
+function ScrollToTopButton() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setShowButton(window.scrollY > 300);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  if (!showButton) {
+    return null;
+  }
+
+  return (
+    <button
+      className="scroll-to-top"
+      onClick={scrollToTop}
+      aria-label="Scroll til toppen"
+      title="Scroll til toppen"
+    >
+      ↑
+    </button>
+  );
+}
+
 export default function App() {
   return (
     <>
       <Navbar />
       <ScrollToTop />
+      <ScrollToTopButton />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -31,7 +67,7 @@ export default function App() {
         <Route path="/tilmeldinger" element={<RegistrationsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      
+
       <Footer />
     </>
   );
